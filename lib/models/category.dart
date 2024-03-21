0405c45f006bd31f589f0d142afab4ae3d001d8d
Anusreepramod <anusreeabhinand@gmail.com>
2024-03-21 16:35:28 +0530

    second commit

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp2/screens/categories.dart';
+
 
 class Category {
   const Category({
-    required this.id,
-    required this.title,
-    this.color =  Colors.orange,
+    required this.id, 
+    required this.title, 
+    this.color = Colors.orange, 
   });
   final String id;
   final String title;
