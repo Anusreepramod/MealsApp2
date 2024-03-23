190e9fbd6f109d57741c0fffd50239c26e111aae
Anusreepramod <anusreeabhinand@gmail.com>
2024-03-23 20:49:49 +0530

    third commmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp2/data/dummy_data.dart';
 import 'package:mealsapp2/screens/meals.dart';
+import 'package:mealsapp2/screens/tabs.dart';
 import 'package:mealsapp2/widgets/category_grid_item.dart';
 import 'package:mealsapp2/models/category.dart';
 
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-  void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
-    .where((meal) => meal.categories.contains(category.id))
-    .toList();
-    
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (ctx) => MealsScreen(
-          title: category.title, 
-          meals: filteredMeals,
-          ),
-    ),
-    );
-  }
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pick your category'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-          ),
-        children:  [
-          for (final category in availableCategories)
-          CategoryGridItem(
-            category: category, 
-            onSelectCategory: () {
-            _selectCategory(context,category);
-          },)
-          
-        ],
-        ),
+    return MaterialApp(
+      theme: theme,
+      home: const TabsScreen(),
     );
   }
-
 }
